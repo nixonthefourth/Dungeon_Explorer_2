09b5007e6a302a1c1ee66b424f17375b321868d1
Nick Khomiakov <72936218+nixonthefourth@users.noreply.github.com>
2025-04-14 15:39:22 +0100

    – Comments –
    Adding comments to the code.

--- DungeonExplorer/Creature.cs
+++ DungeonExplorer/Creature.cs
@@ -7,6 +7,18 @@ namespace DungeonExplorer
         public int CreatureLuck;
         public int CreatureHealth;
 
+        /// <summary>
+        /// Abstract implementation of the getter for creatures.
+        /// </summary>
+        /// 
+        /// <returns>
+        /// Returns string of the name of the assigned creature.
+        /// </returns>
+        ///
+        /// <remarks>
+        /// Player gets the method for defining names.
+        /// Enemy gets a returner.
+        /// </remarks>
         public abstract string GetCreatureName();
     }
 }
--- DungeonExplorer/Player.cs
+++ DungeonExplorer/Player.cs
@@ -2,27 +2,64 @@ namespace DungeonExplorer
 {
     public class Player : Creature
     {
+        /// <summary>
+        /// Constructor for the player.
+        /// </summary>
+        /// 
+        /// <param name="playerName">
+        /// Player's name.
+        /// </param>
+        /// 
+        /// <param name="playerDamage">
+        /// Damage that player is dealing.
+        /// </param>
+        /// 
+        /// <param name="playerLuck">
+        /// Player's luck multiplier.
+        /// </param>
+        /// 
+        /// <param name="playerHealth">
+        /// Player's health parameter.
+        /// </param>
         public Player(string playerName, int playerDamage, int playerLuck, int playerHealth) : base()
         {
-            CreatureName = playerName;
-            CreatureDamage = playerDamage;
-            CreatureLuck = playerLuck;
-            CreatureHealth = playerHealth;
+            playerName = CreatureName;
+            playerDamage = CreatureDamage;
+            playerLuck = CreatureLuck;
+            playerHealth = CreatureHealth;
         }
 
+        /// <summary>
+        /// Gets the player's name. Overrides the abstract method in Creature.
+        /// </summary>
+        /// 
+        /// <returns>
+        /// Name that player has defined.
+        /// </returns>
+        ///
+        /// <remarks>
+        /// Puts the game into the infinite loop, until the input is valid.
+        /// Input is checked on the length, and whether the string is an empty character or a space.
+        /// </remarks>
         public override string GetCreatureName()
         {
             while (true)
             {
-                IHelper.DisplayMessage("Enter your name, mighty warrior: ".ToUpper());
+                // Name input
+                IHelper.DisplayMessage("Enter your name, mighty warrior: ");
                 CreatureName = Console.ReadLine();
-
+                
+                // Input validation
                 if (CreatureName.Length == 0 || CreatureName == "" || CreatureName == " ")
                 {
-                    IHelper.DisplayMessage("Invalid name. \n \n".ToUpper());
-                } else break;
+                    IHelper.DisplayMessage("Invalid name.\n");
+                } 
+                
+                // Successfull case
+                else break;
             }
             
+            // Returns the name of the player.
             return CreatureName;
         }
     }
