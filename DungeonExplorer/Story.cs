e46d9b20f251d867aeeed00659b3a98b4928fa7e
Nick Khomiakov <72936218+nixonthefourth@users.noreply.github.com>
2025-04-14 14:57:08 +0100

    – Story –
    Creating story file with all the story and text lines in the game. In case something else is required to be added – it will be here.

--- DungeonExplorer/Story.cs
+++ DungeonExplorer/Story.cs
@@ -2,6 +2,114 @@ namespace DungeonExplorer
 {
     public class Story : IHelper
     {
+        /// <summary>
+        /// Confirmation sequence (Press enter to continue.)
+        /// </summary>
+        private void ConfirmationMessage()
+        {
+            IHelper.DisplayMessage("\n\nPress Enter to continue...");
+            Console.ReadLine();
+            
+            Console.Clear();
+        }
         
+        /// <summary>
+        /// Displays a welcome message.
+        /// </summary>
+        ///
+        /// <remarks>
+        /// Input line is read and the the screen is cleared later.
+        /// ConfirmationMessage() is applied.
+        /// </remarks>
+        public void Welcome()
+        {
+            IHelper.DisplayMessage("Welcome to Dungeon Explorer!");
+            
+            ConfirmationMessage();
+        }
+
+        /// <summary>
+        /// Shows start of the story.
+        /// </summary>
+        ///
+        /// <remarks>
+        /// Input line is read and the the screen is cleared later.
+        /// ConfirmationMessage() is applied.
+        /// </remarks>
+        public void StoryStart()
+        {
+            IHelper.DisplayMessage("***\n" +
+                                   "Long time ago...\n" +
+                                   "In a far-far galaxy...\n" +
+                                   "Monsters out of Biggleswade were born.\n \n" +
+                                   "It's time for a hero to diabolically demolish them!\n" +
+                                   "***");
+            
+            ConfirmationMessage();
+        }
+        
+        /// <summary>
+        /// Output Character's Name (Name Confirmation Pretty Much)
+        /// </summary>
+        /// 
+        /// <param name="characterName">
+        /// Pushes the name of the character that player defines.
+        /// </param>
+        ///
+        /// <remarks>
+        /// Input line is read and the the screen is cleared later.
+        /// ConfirmationMessage() is applied.
+        /// </remarks>
+        public void CharacterNameConfirmation(string characterName)
+        {
+            IHelper.DisplayMessage($"Welcome, {characterName}!");
+            
+            ConfirmationMessage();
+        }
+        
+        /// <summary>
+        /// Shows the description of the room with randomly selected values
+        /// </summary>
+        public void GetRoomDescription()
+        {
+            Console.Clear();
+            
+            IHelper.DisplayMessage("Would you like to get room description? Y/N ");
+
+            while (true)
+            {
+                string userResponse = Console.ReadLine().ToLower();
+            
+                if (userResponse is "y")
+                {
+                    IHelper.DisplayMessage("I would rather not go back to the old house.");
+                    break;
+                }
+                else if (userResponse is "n") break;
+                else IHelper.DisplayMessage("Invalid response. Please try again: ");
+            }
+        }
+        
+        /// <summary>
+        /// If user loses the adventure.
+        /// </summary>
+        public void LoseAdventure()
+        {
+            Console.Clear();
+            
+            IHelper.DisplayMessage("You are dead, lol");
+            
+            ConfirmationMessage();
+            
+            Environment.Exit(0);
+        }
+        
+        /// <summary>
+        /// User wins the adventure.
+        /// </summary>
+        public void WinAdventure()
+        {
+            IHelper.DisplayMessage("You completed the adventure!");
+        }
     }
 }
